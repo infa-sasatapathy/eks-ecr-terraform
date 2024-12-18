30e6e97464525a4771f5385def8ea781aff83c47
Saumik Satapathy <oracle.saumik@gmail.com>
2024-12-18 17:41:38 +0530

    removed

---
+++ code/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-west-2" # Replace with your preferred region
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.30" # Replace with desired Kubernetes version
+  subnets         = module.vpc.public_subnets
+  vpc_id          = module.vpc.vpc_id
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+
+      instance_type = "t3.medium"
+    }
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name            = "eks-vpc"
+  cidr            = "10.0.0.0/16"
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  enable_nat_gateway = true
+}
